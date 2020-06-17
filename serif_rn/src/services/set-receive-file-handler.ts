def465dcf836d4e9e6cac7e4463193d6cd470147
Ivo Katunaric <ikatun@gmail.com>
2020-06-17 08:28:37 +0200

    Get iOS running without share handler

--- serif_rn/src/services/set-receive-file-handler.ts
+++ serif_rn/src/services/set-receive-file-handler.ts
@@ -1,10 +1,13 @@
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 
 export interface IFileHandlerEvent {
   uris: string[];
 }
 
 export function setReceiveFileHandler(handler: (event: IFileHandlerEvent) => any) {
+  if (Platform.OS === 'ios') {
+    return;
+  }
   const eventEmitter = new NativeEventEmitter(NativeModules.RNHandleShareFileIntentModule);
   eventEmitter.addListener('ShareFileIntent', (args) => {
     handler(args);
